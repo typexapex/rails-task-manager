d0896a77e5088bad60d16e7ded42dfd1c5e41bdf
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-05-18 14:13:24 +0900

    everything is working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,44 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-def index
-  @tasks = Task.all
-end
-
-def show
-  @task = Task.find(params[:id])
-end
-
-def new
-  @task = Task.new
-end
-
-def create
-  @task = Task.new(task_params)
-  if @task.save
-    redirect_to task_path(@task)
-  else
-    render 'new.html.erb'
+  def index
+    @tasks = Task.all
   end
-end
 
-def edit
-  @task = Task.find(params[:id])
-end
-
-def update
-  @task = Task.find(params[:id])
-  if @task.update(task_params)
-    redirect_to task_path(@task)
-  else
-    render 'edit.html.erb'
+  def show
   end
-end
 
-private
+  def new
+    @task = Task.new
+  end
 
-def task_params
-  params.require(:task).permit(:title, :details)
-end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render 'new.html.erb'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit.html.erb'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 
 
 
