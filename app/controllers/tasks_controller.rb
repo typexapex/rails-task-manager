3c7eb7f82c9b9cfb63d081536d0bfd8b0034ae64
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-05-18 13:17:05 +0900

    create is working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,25 @@ def show
   @task = Task.find(params[:id])
 end
 
+def new
+  @task = Task.new
+end
 
+def create
+  @task = Task.new(task_params)
+  if @task.save
+    redirect_to task_path(@task)
+  else
+    render 'new.html.erb'
+  end
+end
+
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details)
+end
 
 
 
